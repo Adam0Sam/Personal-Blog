8c7b6c2da9967ef746e4b1bcc109d582d0f9b3e7
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2023-10-31 17:25:51 +0200

    added footer and tested out json reading functionality

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+//color scheme functionality
 const body = document.querySelector("body");
-const scheme = document.getElementById('scheme');
-if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-    body.classList.add('dark-scheme');
-    scheme.classList.add('dark-scheme');
+const scheme = document.getElementById("scheme");
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  body.classList.add("dark-scheme");
+  scheme.classList.add("dark-scheme");
 }
+scheme.addEventListener("click", () => {
+  body.classList.toggle("dark-scheme");
+  scheme.classList.toggle("dark-scheme");
+});
 
-scheme.addEventListener('click', () => {
-    body.classList.toggle('dark-scheme');
-    scheme.classList.toggle('dark-scheme');
-})
+["./labas.json", "./ate.json"].forEach((fileName) => {
+  fetch(fileName)
+    .then((response) => response.json())
+    .then((result) => console.log(result.content));
+});
