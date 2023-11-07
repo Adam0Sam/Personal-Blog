dc7a888e13df1ee99a1c5ba92e234af0f62b854a
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2023-11-07 08:42:16 +0200

    first draft complete. barebones functionality of rudimentary blog page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-//color scheme functionality
 const body = document.querySelector("body");
 const scheme = document.getElementById("scheme");
+const nextBlog = document.getElementById("next-blog");
+const pageTitle = document.getElementById("title");
+const fileNames = [
+  "./test-jsons/ate.json",
+  "./test-jsons/labas.json",
+  "./test-jsons/big.json",
+];
+
 // if preferred color scheme is dark (light scheme is enabled by defaul via css)
 if (
   window.matchMedia &&
@@ -8,16 +15,56 @@ if (
 ) {
   body.classList.add("dark-scheme");
   scheme.classList.add("dark-scheme");
+  nextBlog.classList.add("dark-scheme");
 }
 scheme.addEventListener("click", () => {
   body.classList.toggle("dark-scheme");
   scheme.classList.toggle("dark-scheme");
+  nextBlog.classList.toggle("dark-scheme");
 });
-//Dynamic sizing functionality
+
+loadBlogPost = (post) => {
+  clear(main);
+  nextBlog.classList.add("active");
+  // quick fix solution to add a new class to header. not permanent
+  document.querySelector("header").classList.add("blog");
+  main.innerHTML = `
+  <div id="blog-container" data-id="${post.id}">
+  <h2 id="blog-title">${post.title}</h2>
+  <div id="blog-details">
+  <p class="activity">${post.activity}</p>
+  <p class="date">${post.date}</p>
+  </div>
+  <div id="blog-content-container">
+  <p id="blog-content">${post.content}</p>
+  </div>
+  </div>
+  </div>
+  `;
+};
+// loadType tells the function if the load request came from the landing page or nextBlog btn
+const fetchBlogPost = (id, requestedFrom = "landingPage") => {
+  if (fileNames[id] == undefined) {
+    console.log("undefined");
+    id = 0;
+    if (requestedFrom == "landingPage") return;
+  }
+
+  fetch(fileNames[id])
+    .then((response) => response.json())
+    .then((result) => loadBlogPost(result));
+  console.log(id);
+};
+
+// create functioning "next blog" button
+nextBlog.addEventListener("click", () => {
+  const currentId = document.querySelector("#blog-container").dataset.id;
+  fetchBlogPost(+currentId + 1, "nextBlog");
+});
+
 const main = document.querySelector("main");
 const initializeLandingPage = () => {
-  main.innerHTML = 
-  `<div id="landingpage-container">
+  main.innerHTML = `<div id="landingpage-container">
   <div class="container"></div>
   </div>`;
 };
@@ -26,37 +73,17 @@ const clear = (element) => {
     element.lastChild.remove();
   }
 };
-const loadBlogPost = (wrapper) => {
-  clear(main);
-  const title = wrapper.children[0].innerText;
-  const content = wrapper.children[1].innerText;
-  const activityType = wrapper.children[2].children[0].innerText;
-  const date = wrapper.children[2].children[1].innerText;
 
-  main.innerHTML = `
-  <div id="blog-container">
-  <h2 id="blog-title">${title}</h2>
-  <div id="blog-details">
-  <p class="activity">${activityType}</p>
-  <p class="date">${date}</p>
-  </div>
-  <div id="blog-content-container">
-  <p id="blog-content">${content}</p>
-  </div>
-  </div>
-  </div>
-  `;
-};
-
-const appendPost = (post, container) => {
+const appendLangingPost = (post, container) => {
   const wrapper = document.createElement("div");
   wrapper.classList.add(`wrapper`);
   wrapper.classList.add(`${post.type}`);
-  wrapper.addEventListener("click", () => loadBlogPost(wrapper));
+  wrapper.dataset.id = `${post.id}`;
   wrapper.dataset.type = post.type;
+  wrapper.addEventListener("click", () => fetchBlogPost(+wrapper.dataset.id));
   wrapper.innerHTML = `
-  <h2 class="header">${post.title}</h2>
-  <p class="subheader">${post.content}</p>
+  <h2 class="blog-header">${post.title}</h2>
+  <p class="blog-subheader">${post.content}</p>
   <div class="details">
   <p class="activity">${post.activity}</p>
   <p class="date">${post.date}</p>
@@ -64,19 +91,19 @@ const appendPost = (post, container) => {
   container.appendChild(wrapper);
 };
 
-const createHeadline = (post) => {
+const createLandingPage = (post) => {
   const landingPage = document.getElementById("landingpage-container");
   const lastChild = landingPage.lastElementChild;
   // if container has no children
   if (!lastChild.hasChildNodes()) {
-    appendPost(post, lastChild);
+    appendLangingPost(post, lastChild);
   }
   // check if only child has a type small and
   else if (
     lastChild.firstElementChild.dataset.type == "small" &&
     post.type == "small"
   ) {
-    appendPost(post, lastChild);
+    appendLangingPost(post, lastChild);
     const newContainer = document.createElement("div");
     newContainer.classList.add("container");
     landingPage.appendChild(newContainer);
@@ -86,10 +113,11 @@ const createHeadline = (post) => {
     const newContainer = document.createElement("div");
     newContainer.classList.add("container");
     landingPage.appendChild(newContainer);
-    appendPost(post, newContainer);
+    appendLangingPost(post, newContainer);
   }
 };
 
+//Dynamic sizing functionality
 const generateRandomHeight = (floor, ceiling, maxHeight) => {
   const min = floor * maxHeight;
   const max = ceiling * maxHeight;
@@ -115,20 +143,26 @@ const loadDynamicHeight = () => {
   });
 };
 
-const fileNames = ["./ate.json", "./labas.json", "./big.json"];
-initializeLandingPage();
-// wrap all filenames into a promise array
-Promise.all(
-  fileNames.map((fileName) =>
-    fetch(fileName)
-      .then((response) => response.json())
-      .then((result) => createHeadline(result))
+const loadLangingPage = () => {
+  initializeLandingPage();
+  nextBlog.classList.remove("active");
+  // wrap all filenames into a promise array
+  Promise.all(
+    fileNames.map((fileName) =>
+      fetch(fileName)
+        .then((response) => response.json())
+        .then((result) => createLandingPage(result))
+    )
   )
-)
-  .then(() => {
-    console.log("All post displayed!");
-    loadDynamicHeight();
-  })
-  .catch((error) => {
-    console.log("Error occured while loading posts: ", error);
-  });
+    .then(() => {
+      console.log("All post displayed!");
+      loadDynamicHeight();
+    })
+    .catch((error) => {
+      console.log("Error occured while loading posts: ", error);
+    });
+};
+loadLangingPage();
+title.addEventListener("click", () => {
+  loadLangingPage();
+});
